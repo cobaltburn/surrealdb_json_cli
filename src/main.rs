187bf5d66c72e5020972c66636ddba8094ca7c65
michael <michaelpederson85@gmail.com>
2024-05-10 19:34:48 -0500

    add ability to export tables as json files or csv files

--- src/main.rs
+++ src/main.rs
@@ -1,160 +1,357 @@
-use anyhow::{Context, Result};
-use clap::Parser;
-use serde_json::value::Value;
-use std::fs;
+use anyhow::{anyhow, Context, Result};
+use clap::{Parser, Subcommand};
+use csv::Writer;
+use serde_json::{Map, Value};
+use std::ffi::OsStr;
 use std::path::PathBuf;
-use surrealdb::engine::remote::ws::{Client, Ws};
+use std::str::FromStr;
+use surrealdb::engine::remote::http::{Client, Http};
 use surrealdb::opt::auth::Root;
-use surrealdb::Surreal;
+use surrealdb::{sql, Surreal};
+use tokio::fs;
+use tokio::io::AsyncWriteExt;
+use tokio::task::JoinSet;
 
-#[derive(Parser, Debug)]
+const ENDPOINT: &str = "127.0.0.1:8000";
+const USER: &str = "root";
+const PASS: &str = "root";
+const NS: &str = "test";
+const DB: &str = "test";
+const FILEFORMAT: &str = "json";
+
+#[derive(Parser)]
 #[command(version, about, long_about = None)]
-struct Args {
-    // #[arg(short, long)]
-    // endpoint: String,
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
 
-    // #[arg(short, long)]
-    // user: String,
+#[derive(Parser)]
+struct Import {
+    #[arg(short, long, default_value = ENDPOINT )]
+    endpoint: String,
 
-    // #[arg(short, long)]
-    // pass: String,
+    #[arg(short, long, default_value = USER)]
+    user: String,
 
-    // #[arg(long)]
-    // ns: String,
+    #[arg(short, long, default_value = PASS)]
+    pass: String,
 
-    // #[arg(long)]
-    // db: String,
-    #[arg(value_name = "FILES", value_parser = clap::value_parser!(PathBuf), num_args = 1.., required = true)]
+    #[arg(long, default_value = NS)]
+    ns: String,
+
+    #[arg(long, default_value = DB)]
+    db: String,
+
+    #[arg(value_name = "files", value_parser = clap::value_parser!(PathBuf), num_args = 1.., required = true)]
     files: Vec<PathBuf>,
 }
 
-enum FileType {
+#[derive(Parser)]
+struct Export {
+    #[arg(short, long, default_value = ENDPOINT )]
+    endpoint: String,
+
+    #[arg(short, long, default_value = USER)]
+    user: String,
+
+    #[arg(short, long, default_value = PASS)]
+    pass: String,
+
+    #[arg(long, default_value = NS)]
+    ns: String,
+
+    #[arg(long, default_value = DB)]
+    db: String,
+
+    #[arg(short, long, default_value = FILEFORMAT)]
+    format: String,
+
+    #[arg(value_name = "tables", value_parser = clap::value_parser!(String), num_args = 1.., required = true)]
+    tables: Vec<String>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Import(Import),
+    Export(Export),
+}
+
+// defaults are for my own sanity
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {}
+
+enum FileFormat {
     Json,
-    Invalid(String),
+    Csv,
+}
+
+impl FromStr for FileFormat {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self> {
+        match s {
+            "json" => Ok(FileFormat::Json),
+            "csv" => Ok(FileFormat::Csv),
+            e => Err(anyhow!("{} is an invalid file format", e)),
+        }
+    }
+}
+
+impl<'a> TryFrom<&'a OsStr> for FileFormat {
+    type Error = anyhow::Error;
+
+    fn try_from(value: &'a OsStr) -> Result<Self> {
+        match value.to_str() {
+            Some("json") => Ok(FileFormat::Json),
+            Some("csv") => Ok(FileFormat::Csv),
+            Some(e) => Err(anyhow!("{} is an invalid file format", e)),
+            None => Err(anyhow!("unable to parser file format")),
+        }
+    }
 }
 
 #[tokio::main]
 async fn main() {
-    let args = Args::parse();
-    println!("{:?}", args);
-
-    let file_type = file_type(&args);
-
-    let db = connect_db("localhost:8000".into(), "root", "root", "test", "test")
-        .await
-        .unwrap();
-
-    match file_type {
-        FileType::Json => {
-            for path in args.files {
-                import_json(path, &db).await.unwrap();
-            }
-        }
-        FileType::Invalid(err) => {
-            eprintln!("{}", err);
-            std::process::exit(1);
-        }
+    let cli = Cli::parse();
+    match cli.command {
+        Commands::Import(args) => import(args).await.unwrap(),
+        Commands::Export(args) => export(args).await.unwrap(),
     }
-}
-
-fn file_type(args: &Args) -> FileType {
-    for path in args.files.iter() {
-        if !path.is_file() {
-            return FileType::Invalid(format!("error: {:?} is not a file", path));
-        };
-        if path.extension().unwrap() != "json" {
-            return FileType::Invalid(format!("error: {:?} is an invalid file type", path));
-        };
-    }
-    let ext = args.files.first().unwrap().extension().unwrap();
-
-    if !args.files.iter().all(|p| p.extension().unwrap() == ext) {
-        return FileType::Invalid(format!("error: not all files are the same type"));
-    }
-
-    FileType::Json
 }
 
 async fn connect_db(
-    address: String,
+    address: &str,
     username: &str,
     password: &str,
     ns: &str,
     db_name: &str,
 ) -> Result<Surreal<Client>> {
-    let db = Surreal::new::<Ws>(address).await?;
-
+    let db = Surreal::new::<Http>(address).await?;
     db.signin(Root { username, password }).await?;
-
     db.use_ns(ns).use_db(db_name).await?;
     Ok(db)
 }
 
-fn generate_query(path: PathBuf) -> Result<(String, Vec<Value>)> {
+async fn import(args: Import) -> Result<()> {
+    let db = connect_db(&args.endpoint, &args.user, &args.pass, &args.ns, &args.db).await?;
+
+    match file_format(&args.files)? {
+        FileFormat::Json => {
+            for path in args.files {
+                import_json(path, &db).await?;
+            }
+        }
+        FileFormat::Csv => todo!(),
+    }
+    Ok(())
+}
+
+fn file_format(files: &Vec<PathBuf>) -> Result<FileFormat> {
+    for path in files.iter() {
+        if !path.is_file() {
+            return Err(anyhow!("error: {:#?} is not a file", path));
+        };
+    }
+    let ext = files
+        .first()
+        .unwrap()
+        .extension()
+        .context("Error: no file extention was found")?;
+
+    if !files.iter().all(|p| p.extension().unwrap() == ext) {
+        return Err(anyhow!("error: not all files are the same type"));
+    }
+
+    FileFormat::try_from(ext)
+}
+
+async fn import_json(path: PathBuf, db: &Surreal<Client>) -> Result<()> {
+    let (insert_query, records) = generate_insert(path).await?;
+    let mut response = db.query(insert_query).bind(("records", &records)).await?;
+    assert!(response.take_errors().is_empty());
+    Ok(())
+}
+
+async fn generate_insert(path: PathBuf) -> Result<(String, Vec<Value>)> {
     let records = {
-        let json = fs::read_to_string(&path)?;
+        let json = fs::read_to_string(&path).await?;
         serde_json::from_str::<Vec<Value>>(&json)
             .unwrap_or(vec![serde_json::from_str::<Value>(&json)?])
     };
+
     let table = path
         .file_stem()
         .context("failed to parse stem")?
         .to_str()
-        .context("failed to conert &OsStr")?;
+        .context("failed to convert &OsStr")?;
 
     let query = format!("INSERT INTO {} $records", table);
     Ok((query, records))
 }
 
-async fn import_json(path: PathBuf, db: &Surreal<Client>) -> Result<()> {
-    let (query, records) = generate_query(path)?;
-    let mut response = db.query(query).bind(("records", &records)).await?;
-    assert!(response.take_errors().is_empty());
+async fn export(args: Export) -> Result<()> {
+    let format = FileFormat::from_str(&args.format)?;
+    let db = connect_db(&args.endpoint, &args.user, &args.pass, &args.ns, &args.db).await?;
+
+    let mut handles = JoinSet::new();
+    for table in args.tables {
+        let db = db.clone();
+        let table = table.clone();
+        match format {
+            FileFormat::Json => {
+                handles.spawn(async move { export_table_as_json(&table, &db).await });
+            }
+            FileFormat::Csv => {
+                handles.spawn(async move { export_table_as_csv(&table, &db).await });
+            }
+        }
+    }
+    while let Some(_) = handles.join_next().await {}
+    Ok(())
+}
+
+async fn export_table_as_json(table: &str, db: &Surreal<Client>) -> Result<()> {
+    let file_name = PathBuf::from(format!("{}.json", table));
+    let records = select_table(table, db).await?;
+
+    let json = serde_json::to_string_pretty(&records)?;
+    let mut file = fs::File::create(file_name).await?;
+    file.write_all(json.as_bytes()).await?;
+    Ok(())
+}
+
+// TODO add check to convert any record ids
+async fn select_table(table: &str, db: &Surreal<Client>) -> Result<Vec<Value>> {
+    let table: Vec<Value> = db.select(table).range(1..2).await?;
+    let table: Vec<Value> = table
+        .into_iter()
+        .map(|mut record| {
+            let Some(record) = convert_id(&mut record) else {
+                return record;
+            };
+            record.to_owned()
+        })
+        .collect();
+    Ok(table)
+}
+
+fn convert_id(record: &mut Value) -> Option<&Value> {
+    let id = record.get_mut("id")?;
+    let thing: sql::Thing = serde_json::from_value(id.clone()).ok()?;
+    *id = Value::String(format!("{}", thing.id));
+    Some(record)
+}
+
+async fn export_table_as_csv(table: &str, db: &Surreal<Client>) -> Result<()> {
+    let records = select_table(table, db).await?;
+    let fields = extract_fields(table, db).await?;
+    let records = records_to_csv(&fields, records);
+
+    let file_name = PathBuf::from(format!("{}.csv", table));
+    let file = std::fs::File::create(file_name)?;
+    let mut wtr = Writer::from_writer(file);
+
+    wtr.write_record(fields)?;
+    for record in records {
+        wtr.write_record(record)?;
+    }
+    wtr.flush()?;
 
     Ok(())
 }
 
+async fn extract_fields(table: &str, db: &Surreal<Client>) -> Result<Vec<String>> {
+    let mut response = db.query(format!("INFO FOR TABLE {}", table)).await?;
+    let mut table = response
+        .take::<Option<Value>>(0)?
+        .context("failed to return table info")?;
+
+    let fields = serde_json::from_value::<Map<String, Value>>(table["fields"].take())?;
+    let fields: Vec<String> = fields.into_iter().map(|(field, _)| field).collect();
+    Ok(fields)
+}
+
+fn records_to_csv(fields: &Vec<String>, records: Vec<Value>) -> Vec<Vec<String>> {
+    let format_record = |rec: Value| {
+        fields
+            .iter()
+            .map(|field| {
+                if let Some(val) = rec.get(field) {
+                    value_to_string(val)
+                } else {
+                    String::from("NULL")
+                }
+            })
+            .collect()
+    };
+
+    records.into_iter().map(format_record).collect()
+}
+
+fn value_to_string(val: &Value) -> String {
+    match val {
+        Value::Null => String::from("NULL"),
+        Value::Bool(e) => e.to_string(),
+        Value::Number(e) => e.to_string(),
+        Value::String(e) => e.to_owned(),
+        Value::Array(e) => serde_json::to_string(e).unwrap(),
+        Value::Object(e) => serde_json::to_string(e).unwrap(),
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use serde_json::json;
-    use surrealdb::{engine::local::Mem, sql::Thing};
-
     use super::*;
+    use serde_json::Map;
+    use surrealdb::sql;
 
     #[tokio::test]
-    async fn test_json_import() {
-        let path = PathBuf::from("./person.json");
+    async fn test_fields() {
+        let db = connect_db(ENDPOINT, USER, PASS, NS, DB).await.unwrap();
+        let mut resp = db.query("INFO FOR TABLE pokemon").await.unwrap();
+        let mut x = resp.take::<Option<Value>>(0).unwrap().unwrap();
+        let x: Map<String, Value> = serde_json::from_value(x["fields"].take()).unwrap();
+        let mut fields = vec![];
+        for (field, _) in x {
+            fields.push(field)
+        }
+        println!("{:?}", fields)
+    }
 
-        let (query, records) = generate_query(path.clone()).unwrap();
+    #[test]
+    fn test_json() {
+        let path = PathBuf::from("./results.json");
+        let json = std::fs::read_to_string(&path).unwrap();
+        let x = sql::json(&json).unwrap();
+        let x: Vec<sql::Value> = Vec::try_from(x).unwrap();
+        x.iter().for_each(|e| println!("{e:#?}"));
+    }
 
-        let db = Surreal::new::<Mem>(()).await.unwrap();
-        db.use_ns("test").use_db("test").await.unwrap();
+    #[tokio::test]
+    async fn test_pull() {}
 
-        let mut response = db.query(query).bind(("records", &records)).await.unwrap();
-        assert!(response.take_errors().is_empty());
+    #[tokio::test]
+    async fn check_db() {
+        let db = connect_db(ENDPOINT, USER, PASS, NS, DB).await.unwrap();
+        let mut resp = db.query(r#"INFO FOR DB"#).await.unwrap();
+        println!("{:?}", resp);
+        let x: Vec<Value> = resp.take(0).unwrap();
 
-        let rec = {
-            let json = fs::read_to_string(&path).unwrap();
-            serde_json::from_str::<Vec<Value>>(&json)
-                .unwrap_or(vec![serde_json::from_str::<Value>(&json).unwrap()])
-        };
+        println!("{:?}", x);
+    }
 
-        let table = path.file_stem().unwrap().to_str().unwrap();
-        let res: Vec<Value> = db.select(table).await.unwrap();
+    #[tokio::test]
+    async fn test_export() {
+        let db = connect_db(ENDPOINT, USER, PASS, NS, DB).await.unwrap();
+        let x = select_table("pokemon", &db).await.unwrap();
+        println!("{:?}", x)
+    }
 
-        let rec: Vec<_> = rec
-            .into_iter()
-            .map(|mut v| {
-                let id = v.get_mut("id").unwrap();
-                let v_id = id.as_i64().unwrap();
-                *id = json!(Thing {
-                    tb: table.into(),
-                    id: v_id.into()
-                });
-                v
-            })
-            .collect();
-
-        assert_eq!(rec, res);
+    #[tokio::test]
+    async fn test_table_save() {
+        let db = connect_db(ENDPOINT, USER, PASS, NS, DB).await.unwrap();
+        export_table_as_json("empty_pokemon", &db).await.unwrap();
     }
 }
